1e1a403c9076b401571ee49b3f4b6c4474e9dfb9
Robbie Kahl <rob.kahl@target.com>
2018-06-20 20:10:34 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -16,15 +16,16 @@ function crazy() {
   }
 thisIsCrazy();
 }
+ 
+ function sayMyName() {
+   // fix the code in here:
+   var name = "Cricky";
+ 
+   sayMy();
+ 
+   function sayMy() {
+     var name = "Kristin";
+     console.log(name);
 
-function sayMyName() {
-@@ -24,7 +25,8 @@ function sayMyName() {
-  sayMy();
-
-  function sayMy() {
-
-    var name = "Kristin";
-   console.log(name);
-
-  }
-}
+   }
+ }
